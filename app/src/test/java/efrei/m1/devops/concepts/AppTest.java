9e7a6f58725f3fd89a6b5660f3678fc8e00ffa4a
Nebsu <115801154+Nebsu@users.noreply.github.com>
2024-09-26 07:02:10 +0000

    test

--- app/src/test/java/efrei/m1/devops/concepts/AppTest.java
+++ app/src/test/java/efrei/m1/devops/concepts/AppTest.java
@@ -17,7 +17,7 @@ class AppTest {
 
     @Test void getGreetingShouldReturnHelloWorld() {
         App classUnderTest = new App();
-        assertEquals("Hello Wold!", classUnderTest.getGreeting(), "getGreeting should return 'Hello World!'");
+        assertEquals("Hello World!", classUnderTest.getGreeting(), "getGreeting should return 'Hello World!'");
     }
 
 }
